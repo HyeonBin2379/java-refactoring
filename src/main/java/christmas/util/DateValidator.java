4eba111314765be5af6bbadd5558fad814cb693f
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-11-10 19:37:22 +0900

    refactor : 유효성 검사 클래스 분리 Validation 클래스를 DateValidator와 OrderValidator로 분리

---
+++ src/main/java/christmas/util/DateValidator.java
@@ -0,0 +1,21 @@
+package christmas.util;
+
+import static christmas.constants.message.ErrorMessage.INVALID_DATE;
+
+public class DateValidator {
+    public static int validateDateInteger(String input) {
+        int valid;
+        try {
+            valid = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_DATE.getErrorMsg());
+        }
+        return valid;
+    }
+
+    public static void validateDateRange(int date) {
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException(INVALID_DATE.getErrorMsg());
+        }
+    }
+}
