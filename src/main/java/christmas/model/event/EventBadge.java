9a6527e9b97ad187804d9bce5c86d22e0c77f741
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-11-11 20:24:16 +0900

    feat(EventBadge) : 혜택 금액에 따른 이벤트 배치 부여 기능 구현

--- src/main/java/christmas/model/event/EventBadge.java
+++ src/main/java/christmas/model/event/EventBadge.java
@@ -1,17 +1,32 @@
 package christmas.model.event;
 
-public enum EventBadge {
-    STAR("별"),
-    TREE("트리"),
-    SANTA("산타"),
-    NONE("없음");
-    private final String badge;
+import java.util.Arrays;
 
-    EventBadge(String badge) {
-        this.badge = badge;
+public enum EventBadge {
+    STAR("별", 5000, 9999),
+    TREE("트리", 10000, 19999),
+    SANTA("산타", 20000, Integer.MAX_VALUE),
+    NONE("없음", 0, 0);
+    private final String name;
+    private final int min;
+    private final int max;
+
+    EventBadge(String name, int min, int max) {
+        this.name = name;
+        this.min = min;
+        this.max = max;
     }
 
-    public String getBadge() {
-        return badge;
+    public static EventBadge getBadge(int totalBenefit) {
+        return Arrays.stream(EventBadge.values())
+                .filter(eventBadge -> eventBadge.isInRange(totalBenefit))
+                .findAny()
+                .orElse(NONE);
+    }
+    private boolean isInRange(int totalBenefit) {
+        return min <= totalBenefit && totalBenefit <= max;
+    }
+    public String getName() {
+        return name;
     }
 }
