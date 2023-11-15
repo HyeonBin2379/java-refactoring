c9ec139124d2b3be69b8ebe2df927009bfe6fba1
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-11-15 23:49:00 +0900

    fix : 출력 메시지 수정

--- src/main/java/christmas/constants/message/MessageFormat.java
+++ src/main/java/christmas/constants/message/MessageFormat.java
@@ -4,7 +4,7 @@ public class MessageFormat {
 
     public static final String ERROR_PREFIX = "[ERROR] ";
     public static final String TITLE_FORMAT = "\n<%s>";
-    public static final String PREVIEW_TITLE_FORMAT = "%d월 %d일에 우테코 식당에서 받을 이벤트 미리보기!\n";
+    public static final String PREVIEW_TITLE_FORMAT = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리보기!\n";
     public static final String MENU_FORMAT = "%s %d개\n";
     public static final String BENEFIT_FORMAT = "%s: %s";
     public static final String MONEY_FORMAT = "%,d원\n";
