fc8011cda486ae275f11ed6d320f26acebd5c125
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-12-23 21:02:15 +0900

    refactor : 연속 근무 시 대체근무자 배정 메소드 분리 setWorker 메소드에서 setWeekdayWorker, setHolidayWorker 메소드 분리

--- java-oncall-6/src/main/java/oncall/model/OnCall.java
+++ java-oncall-6/src/main/java/oncall/model/OnCall.java
@@ -4,40 +4,60 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class OnCall {
-    private final Month month;
-    private final DaysOfWeek[] calender;
+    private final MonthCalender monthCalender;
     private final Worker worker;
-    private final Map<String, String> schedule;
+    private final String[] schedule;
+    private int weekdayIndex, holidayIndex;
     public OnCall(MonthCalender monthCalender, Worker worker) {
-        this.month = monthCalender.getMonth();
-        this.calender = monthCalender.getDaysOnCall();
+        Month month = monthCalender.getMonth();
+        this.monthCalender = monthCalender;
         this.worker = worker;
-        this.schedule = new LinkedHashMap<>();
+        this.schedule = new String[month.getDaysCount()+1];
     }
 
-    public void setOnCall() {
-        int weekdayIndex = 0;
-        int holidayIndex = 0;
-        for (int i = 0; i < calender.length; i++) {
-            String date = getDate(month.getMonth(), i+1);
-            if (isHoliday(month.getMonth(), i+1)) {
-                schedule.put(date, worker.getHolidayWorker(holidayIndex++));
-                continue;
-            }
-            schedule.put(date, worker.getWeekdayWorker(weekdayIndex++));
+    public void setOnCall(Map<String, String> result) {
+        Month month = monthCalender.getMonth();
+        DaysOfWeek[] calender = monthCalender.getDaysOnCall();
+        for (int day = 1; day < calender.length+1; day++) {
+            String date = getDate(month, calender, day);
+            setWorker(month, calender, day);
+            result.put(date, schedule[day]);
         }
     }
-    public String getDate(int month, int day) {
-        String date = String.format("%d월 %d일 %s", month, day, calender[day-1].getDaysOfWeek());
+    public String getDate(Month month, DaysOfWeek[] calender, int day) {
+        String date = String.format("%d월 %d일 %s", month.getMonth(), day, calender[day-1].getDaysOfWeek());
         if (Month.isStationaryHoliday(month, day) && !(calender[day-1].isWeekend())) {
             date += "(휴일)";
         }
         return date;
     }
-    private boolean isHoliday(int month, int day) {
+    public void setWorker(Month month, DaysOfWeek[] calender, int day) {
+        setHolidayWorker(month, calender, day);
+        setWeekdayWorker(month, calender, day);
+    }
+    private void setWeekdayWorker(Month month, DaysOfWeek[] calender, int day) {
+        if (isHoliday(month, calender, day)) {
+            if (day > 1 && schedule[day-1].equals(worker.getHolidayWorker(holidayIndex))) {
+                worker.swapHoliday(holidayIndex, holidayIndex+1);
+            }
+            schedule[day] = worker.getHolidayWorker(holidayIndex++);
+        }
+    }
+    private void setHolidayWorker(Month month, DaysOfWeek[] calender, int day) {
+        if (!isHoliday(month, calender, day)) {
+            if (day > 1 && schedule[day-1].equals(worker.getWeekdayWorker(weekdayIndex))) {
+                worker.swapWeekDay(weekdayIndex, weekdayIndex+1);
+            }
+            schedule[day] =  worker.getWeekdayWorker(weekdayIndex++);
+        }
+    }
+    private boolean isHoliday(Month month, DaysOfWeek[] calender, int day) {
         return Month.isStationaryHoliday(month, day) || calender[day-1].isWeekend();
     }
-    public Map<String, String> getSchedule() {
-        return schedule;
+
+    public Map<String, String> getResult() {
+        Map<String, String> result = new LinkedHashMap<>();
+        setOnCall(result);
+        return result;
     }
 }
