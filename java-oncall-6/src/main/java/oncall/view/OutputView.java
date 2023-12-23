b97879847c84fbf03ac5ed72db2dfa2396cb2fda
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-12-23 18:42:56 +0900

    feat : 입출력 기능 구현

--- java-oncall-6/src/main/java/oncall/view/OutputView.java
+++ java-oncall-6/src/main/java/oncall/view/OutputView.java
@@ -1,27 +1,11 @@
 package oncall.view;
 
-import oncall.model.DaysOfWeek;
-import oncall.model.Month;
-import oncall.model.MonthCalender;
-import oncall.model.StationaryHolidays;
+import java.util.Map;
 
 public class OutputView {
-    public void printOnCall(MonthCalender monthCalender) {
-        System.out.println();
-        Month month = monthCalender.getMonth();
-        DaysOfWeek[] result = monthCalender.getDaysOnCall();
-        for (int i = 1; i < result.length+1; i++) {
-            System.out.printf("%d월 %d일 %s", month.getMonth(), i, result[i-1].getDaysOfWeek());
-            if (isStationHoliday(month, i) && isWeekday(result[i-1])) {
-                System.out.print("(휴일)");
-            }
-            System.out.println();
+    public void printOnCall(Map<String, String> schedule) {
+        for (Map.Entry<String, String> entry : schedule.entrySet()) {
+            System.out.printf("%s %s" + System.lineSeparator(), entry.getKey(), entry.getValue());
         }
-    }
-    private boolean isStationHoliday(Month month, int day) {
-        return StationaryHolidays.findHoliday(month, day) != StationaryHolidays.NONE;
-    }
-    private boolean isWeekday(DaysOfWeek daysOfWeek) {
-        return daysOfWeek != DaysOfWeek.SAT && daysOfWeek != DaysOfWeek.SUN;
     }
 }
